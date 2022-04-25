23c9b4b581a8ff0fb75e0ee88521fa1ea0fabfbd
Michael Bishop <michaelabishop1988@gmail.com>
2022-04-25 14:11:56 +0100

    completed tasks

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 const yargs = require("yargs")
 
-const { Movie, movieArr } = require("./utils")
+// const { Movie, movieArr } = require("./utils")
+
+const { Food, foodArr } = require("./utils")
 
 // console.log(yargs.argv)
 
 const app = (yargsObj) => {
+    // if (yargsObj.add) {
+    //     const movie = new Movie(yargsObj.title, yargsObj.actor, yargsObj.genre, yargsObj.rating)
+    //     movie.add()
+    //     console.log(movieArr)
+    // } else {
+    //     console.log("Incorrect Command")
+    // }
+
     if (yargsObj.add) {
-        const movie = new Movie(yargsObj.title, yargsObj.actor)
-        movie.add()
-        console.log(movieArr)
+        const newFood = new Food(yargsObj.food, yargsObj.price, yargsObj.color, yargsObj.isGood)
+        newFood.addFood()
+        console.log(newFood)
     } else {
         console.log("Incorrect Command")
     }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,20 +1,40 @@
 // create a empty array to store data
-const movieArr = []
+// const movieArr = []
+
+const foodArr = []
 
 // create Move class for title and actor object
 // 'this' keyword refers to the object it belongs to 
-class Movie {
-    constructor(title, actor = "Not specified") {
-        this.title = title
-        this.actor = actor
+// class Movie {
+//     constructor(title, actor = "Not specified", genre, rating) {
+//         this.title = title
+//         this.actor = actor
+//         this.genre = genre
+//         this.rating = rating
+//     }
+//     // small function to add cli input to the movieArr
+//     add() {
+//         movieArr.push(this)
+//     }
+// }
+
+class Food {
+    constructor(food, price, color, isGood) {
+        this.food = food
+        this.price = price
+        this.color = color
+        this.isGood = isGood
     }
-    // small function to add cli input to the movieArr
-    add() {
-        movieArr.push(this)
+
+    addFood () {
+        foodArr.push(this)
     }
 }
 
 module.exports = {
-    Movie,
-    movieArr
+    // Movie,
+    // movieArr
+
+    Food,
+    foodArr
 }
